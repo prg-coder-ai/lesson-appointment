64d8a1f973dd36558024b310b34b841816c96573
prg-coder-ai <229845331@qq.com>
2026-04-10 22:55:08 -0600

    ----课程编辑--对话框没有显示。课程列表正确显示。

--- zktapi-master/src/main/kotlin/com/ih/api/model/Alarmconfig.kt
+++
@@ -1,4 +0,0 @@
-package com.ih.api.model
-
-/* 报警设置 */
-data class Alarmconfig(var id: Int = -1, var lowAlarm2: Double = 0.0, var lowAlarm1: Double = 0.0, var highAlarm1: Double = 0.0, var highAlarm2: Double = 0.0, var color_low2: String = "", var color_low1: String = "", var color_Normal: String = "", var color_High1: String = "", var color_High2: String = "")
--- zktapi-master/src/main/kotlin/com/ih/api/model/Heatexchangerunit.kt
+++
@@ -1,6 +0,0 @@
-package com.ih.api.model
-
-import java.util.*
-
-/* 换热站机组模型 */
-data class Heatexchangerunit(var id: Int = -1, var stationID: Int = -1, var unitNumber: String = "", var createTime: Date = Date(), var createrID: Int = -1, var address: String = "", var stationName: String = "", var companyID: Int = 0, var companyName: String = "",var crewID:String="",var updateDesc:String="")
