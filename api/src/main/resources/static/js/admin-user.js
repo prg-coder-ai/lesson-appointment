8350adf6ebd9c9091910caa2416673e37457cf92
prg-coder-ai <229845331@qq.com>
2026-07-25 01:01:25 -0600

    cc

--- api/src/main/resources/static/js/admin-user.js
+++ api/src/main/resources/static/js/admin-user.js
@@ -5,43 +5,45 @@
       total: 0,   // 总条数
       totalPages: 0 // // 总页数
     };
-
+   // 引入分页组件js
+   document.write('<script src="/js/public/pagefoot.js"></script>');
+   
+   window.renderTeacherCards = renderTeacherCards;
+   let currentUserRole ="";
    async function renderTeacherCards(role) {
-            //  dynamicContentCenter.innerHTML = '<div style="padding:40px 0;text-align:center;">加载中...</div>';
-            //  console.log("enter");
-            //  const conditionJson = { role: 'teacher' };//TBD:当前admin所属的群组等过滤条件
-            //  const teachers = await fetchUserList(conditionJson);
-             // console.log("list:"+teachers);
-              let html = `
+           currentUserRole     = role;
+           assignLoadobjectListFunction( loadAndRenderUserList);
+               let html = `
                 <div class="card">
                   <div class="card-header">
                     <div class="card-title"><i class="fa fa-chalkboard-teacher"></i> ${role=="teacher"?"教师列表":"学生列表" } </div>
-                    <button class="btn btn-primary" onclick="alert('TODO: 打开添加教师弹窗')"><i class="fa fa-plus"></i> 添加教师</button>
+                    <button class="btn btn-primary" onclick="alert('TODO: 打开添加用户弹窗')"><i class="fa fa-plus"></i> 添加用户</button>
                   </div>
                   <div class="teacher-list-cards" style="margin:6px 0;display:flex;flex-direction:column;gap:16px;">
                
               <!-- 筛选条件 -->
               <div class="filter-bar">  
-                <div class="filter-item">
-                  <label  style="width:0px;display:none" >角色：</label>
-                  <input type="text" style="width:0px;display:none"  id="user-role-input" ,value = ${role}>
-                </div>
+                <!-- div class="filter-item">
+                  <label  style="display:none" >角色：</label>
+                  <input type="text" style="display:none"  id="user-role-input" ,value = ${role}>
+                </div  -->
 
                 <div class="filter-item">
                   <label>姓名：</label>
                   <input type="text" id="user-name-input" placeholder="姓名">
                 </div>
+                                 <div class="filter-item">
+                   <label>账号</label>
+                  <input type="text" id="user-account-input" placeholder="账号">
+                </div>
+
                 <div class="filter-item">
                    <label>电子邮件：</label>
-                  <input type="text" id="user-email-input" placeholder="xx@xx">
+                  <input type="text" id="user-email-input" placeholder="email">
                 </div>
                 <div class="filter-item">
                    <label>电话号码：</label>
-                  <input type="text" id="user-phone-input" placeholder="(8610)1361234567">
-                </div>
-                 <div class="filter-item">
-                   <label>账号</label>
-                  <input type="text" id="user-account-input" placeholder="账号">
+                  <input type="text" id="user-phone-input" placeholder="phonenumber">
                 </div>
 
                 <div class="filter-item">
@@ -49,7 +51,9 @@
                   <select id="user-status-select">
                     <option value="">全部</option>
                     <option value="active">有效</option>
-                    <option value="pending">挂起</option>
+                    <option value="pending">待审核</option>
+                    <option value="frozen">冻结</option>
+                    <option value="delete">已删除</option>
                   </select>
                 </div> 
                 <button class="btn btn-default" onclick="localsearchUsers()">
@@ -59,7 +63,7 @@
                   <i class="fa fa-redo"></i> 重置
                 </button>
               </div> 
-
+             
                <!-- 数据表格 -->
               <div class="table-container">
                 <table class="data-table">
@@ -68,14 +72,14 @@
                     <th>序号</th>  
                     <th style="width:0px;display:none">Id</th>
                     <th>姓名</th>  
-                    <th>电子邮箱</th> 
-                    <th>电话号码</th>
                     <th>账号</th> 
+                    <th>电子邮箱</th> 
+                    <th>电话号码</th>                    
                     <th>状态</th>
                     <th>操作</th>
                 </tr>
                 </thead>
-                <tbody id="course-table-body">
+                <tbody id="user-table-body">
                 <!-- 数据由JS动态渲染 -->
                  </tbody>
               </table>
@@ -83,29 +87,18 @@
                 `;             
               
           // <!-- 分页栏 -->
-            html += `
-            <div class="pagination-bar">
-              <div class="pagination-info">
-                共 <span id="course-total">0</span> 条记录，每页 
-                <select id="course-page-size" onchange="changeCoursePageSize()">
-                  <option value="5" selected>5</option>
-                  <option value="10" selected>10</option>
-                  <option value="20">20</option>
-                  <option value="50">50</option>
-                </select> 条
-              </div>
-              <div class="pagination-btns" id="course-pagination-btns"></div>
-            </div>
+            html += getPagebar();//defined in pageFoot.js
+            html += `          
           </div>
           `;
               dynamicContentCenter.innerHTML = html;
-              loadUserList("teacher");
+              loadUserList(role);//"teacher");
             }
+
 
             function localsearchUsers() {
               Pagination.pageNum = 1;
-              const role = document.getElementById('user-role-input').value.trim();//
-              loadUserList(role);
+              loadAndRenderUserList();
             }
               // 重置筛选条件
         function resetUserFilter() {
@@ -114,48 +107,20 @@
             document.getElementById('user-phone-input').value = '';
             document.getElementById('user-account-input').value = '';
             document.getElementById('user-status-select').value = '';
-            coursePagination.pageNum = 1;
-            const role = document.getElementById('user-role-input').value.trim();//
-            loadUserList(role);
+            Pagination.pageNum = 1;
+           loadAndRenderUserList();
         }
 
 
         
 // ===================== 交互函数 =====================
-/**
- * 筛选条件变化
- */
-async function handleSearchChange() {
-  localParamter.currentPage = 1; // 重置页码
-  await renderCourseCards();
-}
+           function loadAndRenderUserList(){
+            //const role = document.getElementById('user-role-input').value.trim();//
+            //console.error("role",role);
+             loadUserList(currentUserRole);
+           } 
 
-
-function changeCourseStatus(courseId, status) { 
-operateCourse(courseId, status);
-loadCourseList();
-
-} 
- 
-
-/**
-* 分页大小变化
-*/
-async function handlePageSizeChange(val) {
-  localParamter.pageSize = val;
-  await renderCourseCards();
-}
-
-/**
-* 页码变化
-*/
-async function handleCurrentPageChange(val) {
-  localParamter.currentPage = val;
-  await renderCourseCards();
-}
-
-        async function loadUserList(role){
-                      
+      async function loadUserList(role){                      
             const params = new URLSearchParams({
               pageNum:  Pagination.pageNum,
               pageSize: Pagination.pageSize,
@@ -180,8 +145,8 @@ async function handleCurrentPageChange(val) {
                 
                 // 渲染表格
                 renderUserTable(pageData.rows);
-                // 渲染分页栏
-                renderPagination();
+                // 渲染分页栏,带入分页参数
+                renderPagination( Pagination);
               }
             } catch (error) {
               console.error('加载列表失败：', error);
@@ -189,46 +154,67 @@ async function handleCurrentPageChange(val) {
           }
   
           function renderUserTable(userList){
-
-            teachers.forEach(tea => {
+                
+            const tbody = document.getElementById('user-table-body');
+         //   console.error("1 tbody",tbody);
+         //   console.error("2 tbody",userList);
+            if (userList.length === 0) {
+              tbody.innerHTML = '<tr><td colspan="8" style="text-align:center;color:#999;padding:40px 0;">暂无数据</td></tr>';
+              return;
+            }
+             let  html  = ` `;
+            var index=(Pagination.pageNum-1)*Pagination.pageSize;//记录序号
+            
+            userList.forEach(tea => {
+              index ++;
               html += `
-                <div class="teacher-card" style="background:#fff;border:1px solid #e9ecef;padding:16px;border-radius:8px;display:flex;align-items:center;justify-content:space-between;box-shadow:0 2px 8px #0000000a;">
-                  <div style="display:flex;gap:36px;">
-                    <div style="display:none;">${tea.userId || ""}</div>
-                    <div style="width:90px;" title="${tea.userId || ''}" >${tea.name || ""}</div>
-                    <div style="width:180px;">${tea.email || ""}</div>
-                    <div style="width:130px;">${tea.phone || ""}</div>
-                    <div style="width:120px;">
+                    <tr>
+                     <td> ${index} </td>
+                     <td style="display:none;">${tea.userId || ""}</td>
+                    <td>${tea.name || ""}</td>
+                    <td>${tea.account || ""}</td>
+                    <td>${tea.email || ""}</td>
+                    <td>${tea.phone || ""}</td>
+                    <td>
                       ${ tea.status === "pending" ? '<span style="color:#faad14;">待审核</span>' :
-                        tea.status === "active" ? '<span style="color:#52c41a;">正常</span>' :
-                        tea.status === "inactive" ? '<span style="color:#faad14;">待启用</span>' :
-                        tea.status === "frozen" ? '<span style="color:#f5222d;">已删除</span>' :
+                        tea.status === "active" ? '<span style="color:#52c41a;">正常</span>' :                        
+                        tea.status === "frozen" ? '<span style="color:#f5222d;">冻结</span>' :
+                        tea.status === "inactive" ? '<span style="color:#faad14;">失效</span>' :
+                        tea.status === "delete" ? '<span style="color:#faad14;">已删除</span>' :
                         `<span>${tea.status||"未知"}</span>`
                       }
-                    </div>
-                  </div>
-                  <div class="teacher-actions" style="display:flex;gap:10px;flex-shrink:0;">
+                      </td>
+                  
+                  <td>
                     <button class="btn btn-success"  onclick="confirmTeacher('${tea.userId}', '${tea.role}')"><i class="fa fa-check"></i> 启用</button>
                     <button class="btn btn-warning" onclick="disableTeacher('${tea.userId}', '${tea.role}')"><i class="fa fa-ban"></i> 冻结</button>
                     <button class="btn btn-danger" onclick="deleteTeacher('${tea.userId}', '${tea.role}')"><i class="fa fa-trash"></i> 删除</button>
                     <button class="btn btn-warning" onclick="resetUserPasswd('${tea.userId}')"><i class="fa fa-trash"></i> 重置密码</button>
-                  </div>
-                </div>
+                  </td>
+                </tr>
               `;
              })
-  
+             tbody.innerHTML = html;
           }
+          //利用changepasswordapi设置用户密码为缺省值
+    function resetUserPasswd(userId) { 
+      if (!confirm("确定要重置此用户的密码为默认值吗？")) {
+        return;
+      }
+      changePasswordAPI(userId,"123456");
+    }
+
                //禁用
                 async  function disableTeacher(userId,role) {
                           // 调用 update(User) 把 status 设置为 inactive
-                          const user = {userId: userId, status: "inactive" };
+                          const user = {userId: userId, status: "frozen" };
                           request({
                             url: `${API_BASE_URL}/user/updateStatus`,
                             method: 'POST',
                             data: user
                           }).then(res => {
 
-                              renderCards(role);
+                              loadUserList(role);
                           }).catch(e => {
                             alert("网络错误，禁用失败");
                           });
@@ -242,7 +228,7 @@ async function handleCurrentPageChange(val) {
                             method: 'POST',
                             data: { userId: userId, status: "active" }
                           }).then(res => {
-                              renderCards(role);
+                              loadUserList(role);
                           }).catch(e => {
                             alert("网络错误，确认失败");
                           });
@@ -254,10 +240,10 @@ async function handleCurrentPageChange(val) {
                             request({
                               url: `${API_BASE_URL}/user/updateStatus`,
                               method: 'POST',
-                              data: { userId: userId, status: "frozen" }
+                              data: { userId: userId, status: "delete" }
                             }).then(res => {
                                 //  刷新列表
-                                renderCards(role);
+                                loadUserList(role);
                             }).catch(e => {
                               alert("网络错误，删除失败");
                             });
